9470d072809bd859800edfa220165f509f26840e
Pamela Iman <pamelariman@gmail.com>
2024-06-03 14:44:58 +0100

    checking CI results on GitHub with failing tests from VS

---
+++ src/find-shortest-string.test.js
@@ -0,0 +1,13 @@
+// import function from local file
+const findShortestString = require("./find-shortest-string");
+
+test("findShortestStringfinds the shortest string in an array", () => {
+  expect(findShortestString(["it", "is", "a", "nice", "day"])).toBe("a");
+  expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
+});
+
+test("findShortestString returns the earlier string in cases of joint shortest strings", () => {
+  expect(findShortestString(["brave", "dance"])).toBe("brave");
+  expect(findShortestString(["praise", "God", "Amen", "banana", "hey"])).toBe("God");
+  expect(findShortestString(["abc", "defg", "hi", "jk"])).toBe("hi");
+});
